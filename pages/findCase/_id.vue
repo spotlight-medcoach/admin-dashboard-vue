3ec427a04458748d9abaa929ba6c2efe80e1b6f1
LunaMan01 <luis-luna98@hotmail.com>
2021-06-10 13:55:02 -0500

    view completed, case details are missing

---
+++ pages/findCase/_id.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <Navigation />
+        <h1>{{this.$route.params.id}}</h1>
+    </div>
+</template>
+
+<script>
+import Navigation from '../../components/navs/Navigation';
+
+export default {
+    components: {
+        Navigation
+    }
+}
+</script>
+
+<style>
+
+</style>
