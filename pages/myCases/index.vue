a043d677c15600358018248601ca9178e9532b31
LunaMan01 <luis-luna98@hotmail.com>
2021-06-10 22:06:51 -0500

    spotlighter view

--- pages/myCases/index.vue
+++ pages/myCases/index.vue
@@ -9,8 +9,8 @@
                 </div>
 
                 <div v-if="!loading" class="pay-container">
-                    <p>Fecha de último pago</p>
-                    <button class="btn"><i class="fas fa-dollar-sign"></i> Solicitar pago</button>
+                    <p>Fecha de último pago...</p>
+                    <button class="btn" @click="requestPaymentConfirm"><i class="fas fa-dollar-sign"></i> Solicitar pago</button>
                 </div>
 
             </div>
@@ -26,7 +26,7 @@
                     <option value="Pending review">Revisión pendiente</option>
                     <option value="In edit">En edición</option>
                     <option value="With feedback">Feedback</option>
-                    <option value="Approved by Spotlighter">Aprobados</option>
+                    <option value="Approved">Aprobados</option>
                 </select>
 
                 <select v-model="topicSelected" name="topic" class="js-example-basic-single" @change="filterSubtopics(topicSelected)">
@@ -60,7 +60,12 @@
                             <td>{{ theCase.topic_name }}</td>
                             <td>{{ theCase.subtopic_name }}</td>
                             <td>{{ theCase.request_description.content }}</td>
-                            <td>{{ theCase.status }}</td>
+                            
+                            <td v-if="theCase.status == 'In edit'"><div class="edit">En edición</div></td>
+                            <td v-else-if="theCase.status == 'Pending review'"><div class="review">Revisión pendiente</div></td>
+                            <td v-else-if="theCase.status == 'With feedback'"><div class="feedback">Feedback</div></td>
+                            <td v-else-if="theCase.status == 'Approved'"><div class="approved">Aprovado</div></td>
+
                             <td class="td-style">
                                 <button class="btn op" @click="viewCase(myCases[index])"><i class="fas fa-list-alt"></i> Ver caso</button>
                             </td>
@@ -80,33 +85,48 @@
                         <option value=10>10</option>
                         <option value=15>15</option>
                         <option value=20>20</option>
-                        <option value=40>40</option>
-                        <option value=50>50</option>
                     </select>
                 </div>
 
                 <div class="arrows-container">
-                    <span>1 - {{pageResults}} of {{totalCases}} casos</span>
-                    <button class="btn fas fa-chevron-left" @click="before"></button>
-                    <button class="btn fas fa-chevron-right" @click="after"></button>
+                    <span>{{ (page - 1) * pageResults + 1 }} - {{ pageResults > totalCases ? totalCases : (page * pageResults) > totalCases ? totalCases : page * pageResults }} of {{totalCases}} casos</span>
+                    <button class="btn fas fa-chevron-left" @click="before" :disabled="disbaledBefore == 1"></button>
+                    <button class="btn fas fa-chevron-right" @click="after" :disabled="disabledAfter == 1"></button>
                 </div>
             </div>
         </div>
+
+        <RequestPayment
+            v-if="isShowPaymentModal"
+            @close="closePaymentModal"
+            :textTitle="titleModal"
+            :textBody="bodyModal"
+            :isBusy="busyPayment"
+            :action="requestPayment"
+            :textButton="button"
+            questions="123"
+            payment="123" />
+
     </div>
 </template>
 
 <script>
 import SpotlighterNavigation from '../../components/navs/SpotlighterNavigation';
 import Loading from '../../components/modals/Loading';
+import RequestPayment from '../../components/modals/RequestPayment';
 
 export default {
     components: {
         SpotlighterNavigation,
-        Loading
+        Loading,
+        RequestPayment
     },
     data() {
         return {
             loading: false,
+            isShowPaymentModal: false,
+            busyPayment: false,
+
             myCases: [],
             topics: [],
             subtopics: [],
@@ -116,8 +136,12 @@ export default {
             topicBubbleSelected: '',
             subtopicBubbleSelected: '',
 
+            titleModal: '',
+            bodyModal: '',
+            button: '',
+
             totalCases: 0,
-            pageResults: 50,
+            pageResults: 5,
             page: 1
         }
     },
@@ -181,17 +205,69 @@ export default {
         addCase() {
             this.$router.push({ path: '/myCases/addNewCase' });
         },
+        requestPaymentConfirm() {
+            this.titleModal = 'Solicitar pago'
+            this.bodyModal = '¿Deseas solicitar el pago de este periodo? Al realizar esta acción el contador quedará en ceros al igual que tu saldo pendiente y se reiniciará el conteo de preguntas.'
+            this.button = 'Solicitar pago'
+            
+            this.isShowPaymentModal = !this.isShowPaymentModal;
+        },
+        async requestPayment() {
+            this.busyPayment = !this.busyPayment;
+
+            setTimeout(() => {
+                alert('Make request payment');
+                this.busyPayment = !this.busyPayment;
+                // this.isShowPaymentModal = !this.isShowPaymentModal;
+            }, 1500)
+        },
         rowsChange() {
+            this.page = 1;
+
+            if (this.pageResults > this.totalCases) {
+                this.disabledAfter = 1
+                this.disbaledBefore = 1
+            } else {
+                this.disbaledBefore = 1
+                this.disabledAfter = 0
+            }
+
             this.getMyCases()
         },
         before() {
-            alert('Logica para esta asunto')
+            if (this.page == 1) {
+                this.disbaledBefore = 1
+                if (this.totalCases == 0 || this.pageResults > this.totalCases)
+                    this.disabledAfter = 1
+            } else if (this.page > 1) {
+                this.page -= 1;
+                this.disabledAfter = 0
+
+                if (this.page == 1)
+                    this.disbaledBefore = 1
+            }
+
+            this.getMyCases();
         },
         after() {
-            alert('Logica para esta asunto')
+            this.page += 1;
+            if (this.page * this.pageResults > this.totalCases) {
+                this.disabledAfter = 1
+                this.disbaledBefore = 0
+            } else {
+                if (this.page > 1)
+                    this.disbaledBefore = 0
+                    if ((this.page * this.pageResults) >= this.totalCases)
+                        this.disabledAfter = 1
+            }
+
+            this.getMyCases();
         },
         viewCase(theCase) {
             this.$router.push({ path: `/myCases/${theCase._id}` });
+        },
+        closePaymentModal() {
+            this.isShowPaymentModal = false;
         }
     }
 }
@@ -315,6 +391,38 @@ export default {
 
     td {
         vertical-align: middle;
+    }
+
+    .edit {
+        font-weight: bold;
+        text-align: center;
+        background: #FFDE89;
+        border-radius: 4px;
+        padding: 4px 12px;
+    }
+
+    .review {
+        font-weight: bold;
+        text-align: center;
+        background: #b4b4b4;
+        border-radius: 4px;
+        padding: 4px 12px;
+    }
+
+    .feedback {
+        font-weight: bold;
+        text-align: center;
+        background: #61C0FD;
+        border-radius: 4px;
+        padding: 4px 12px;
+    }
+
+    .approved {
+        font-weight: bold;
+        text-align: center;
+        background: #90DF7E;
+        border-radius: 4px;
+        padding: 4px 12px;
     }
 
     .op {
