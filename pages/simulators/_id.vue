a239ca98bb871427ad83a6fb8fcb95da2c26ebec
LunaMan01 <luis-luna98@hotmail.com>
2021-06-18 18:56:49 -0500

    simulators list

---
+++ pages/simulators/_id.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <Navigation />
+
+        <div class="sim-container">
+            <h1>{{this.$route.params.id}}</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+import Navigation from '../../components/navs/Navigation';
+
+export default {
+    components: {
+        Navigation
+    }
+}
+</script>
+
+<style scoped>
+    .sim-container {
+        display: flex;
+        flex-direction: column;
+        font-family: Montserrat;
+    }
+</style>
--- pages/simulators/index.vue
+++ pages/simulators/index.vue
@@ -1,20 +1,89 @@
 <template>
     <div>
         <Navigation />
-        <h1>Simuladores</h1>
+        <div class="simulator-container">
+            <h1>Simuladores</h1>
+
+            <Loading v-if="loading" />
+            <div v-else class="cards-container">
+                <SimulatorCard
+                    v-for="simulator in simulators"
+                    :key="simulator._id"
+                    :title="simulator.name"
+                    :id="simulator._id"
+                    :totalQuestions="simulator.questions.length"
+                    :status="simulator.enabled"
+                    @view="simulatorDetails(simulator)" />
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import Navigation from '../../components/navs/Navigation';
+import Loading from '../../components/modals/Loading';
+import SimulatorCard from '../../components/cards/administrators/SimulatorCard';
 
 export default {
     components: {
-        Navigation
+        Navigation,
+        Loading,
+        SimulatorCard
+    },
+    data() {
+        return {
+            loading: false,
+
+            simulators: []
+        }
+    },
+    async created() {
+        if (process.browser) {
+            this.$axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('user_token')}`
+            // this.topics = JSON.parse(localStorage.getItem('topics'));
+        }
+
+        await this.getSimulators();
+        console.log(this.simulators)
+    },
+    methods: {
+        async getSimulators() {
+            try {
+                this.loading = !this.loading;
+
+                let simulatorsResponse = await this.$axios.get('/getAllSimulators');
+                this.simulators = simulatorsResponse.data.payload;
+                
+                this.loading = !this.loading;
+            } catch (err) {
+                console.log(err);
+            }
+        },
+        simulatorDetails(simulator) {
+            this.$router.push({ path: `/simulators/${simulator._id}` });
+        }
     }
 }
 </script>
 
 <style scoped>
+    .simulator-container {
+        display: flex;
+        flex-direction: column;
+        font-family: Montserrat;
+        margin: 20px 40px;
+    }
 
+    .simulator-container h1 {
+        font-style: normal;
+        font-weight: bold;
+        font-size: 32px;
+        line-height: 39px;
+        color: #000000;
+    }
+
+    .cards-container {
+        display: flex;
+        flex-direction: column;
+    }
 </style>
